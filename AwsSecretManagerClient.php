b2d3f505b4de31849b4b98df2e8214b9a46ad902
skStavir <santhosh.krishna@stavir.com>
2024-03-27 12:44:52 +0530

    added a comment

--- AwsSecretManagerClient.php
+++ AwsSecretManagerClient.php
@@ -10,6 +10,12 @@ $region = 'ap-southeast-2';
 $client = new SecretsManagerClient([
     'version' => 'latest',
     'region'  => $region,
+    /*
+    for the actual code we will get this from env varibales 
+
+    $ export AWS_ACCESS_KEY_ID=abc # The access key for your AWS account.
+    $ export AWS_SECRET_ACCESS_KEY=xyz # The secret access key for your AWS account.
+    */
     'credentials' => [
         'key'    => 'ConfigureActualKey',
         'secret' => 'ConfigureActualSecret',
